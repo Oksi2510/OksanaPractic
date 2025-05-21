9455ff8a9926de68d4cd3e0700ecc26eb4bd8d8a
Oksana Mirasova <mirasovaoksana@yandex.ru>
2025-05-21 19:19:45 +0700

    Добавлен раздел с командой

--- JS/main.js
+++ JS/main.js
@@ -1,12 +1,32 @@
-$(function() {
+$(function () {
   Fancybox.bind("[data-fancybox]", {
     // опции
   });
 
-  var mixer = mixitup('.directions__list');
+  var mixer = mixitup(".directions__list");
 
-  $('.direction__filter-btn').on('click', function () {
-    $('.direction__filter-btn').removeClass('direction__filter-btn--active');
-    $(this).addClass('direction__filter-btn--active');
+  $(".direction__filter-btn").on("click", function () {
+    $(".direction__filter-btn").removeClass("direction__filter-btn--active");
+    $(this).addClass("direction__filter-btn--active");
   });
+
+  $(".team__slider").slick({
+    arrows: false,
+    slidesToShow: 4,
+    infinite: true,
+    speed: 300,
+    draggable: false,
+  });
+  $(".team__slide-prev").on("click", function (e) {
+    e.preventDefault();
+    $(".team__slider").slick("slickPrev");
+  });
+
+  $(".team__slide-next").on("click", function (e) {
+    e.preventDefault();
+    $(".team__slider").slick("slickNext");
+  });
+
+
+  
 });
