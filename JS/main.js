8e7dcbfd1d54875e9a1757d23644a742e318e48f
Oksana Mirasova <mirasovaoksana@yandex.ru>
2025-05-25 14:51:26 +0700

    Добавленна секция PROGRAM

--- JS/main.js
+++ JS/main.js
@@ -45,4 +45,22 @@ $(function () {
     e.preventDefault();
     $(".testimonials__slider").slick("slickNext");
   });
+
+  // $(".program__acc__link").on("click", function (e) {
+  //   e.preventDefault();
+  //   $(this).toggleClass("program__acc__link--active");
+  //   $(this).children(".program__acc-text").slideToggle(300); // 300 — это длительность в мс
+  // });
+  $(".program__acc__link").on("click", function (e) {
+    e.preventDefault();
+    if ($(this).hasClass("program__acc__link--active")) {
+      $(this).removeClass("program__acc__link--active");
+      $(this).children(".program__acc-text").slideUp();
+    } else {
+      $(".program__acc__link").removeClass("program__acc__link--active");
+      $(".program__acc-text").slideUp();
+      $(this).addClass("program__acc__link--active");
+      $(this).children(".program__acc-text").slideDown();
+    }
+  });
 });
